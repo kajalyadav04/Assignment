e2c7031c4f0a4c85acfb3c962c84703df27431d4
kajalyadav04 <kajalraj1520@gmail.com>
2024-10-12 11:25:18 +0530

    implemented calculate formula function

--- Java-CSV/src/com/java/Main.java
+++ Java-CSV/src/com/java/Main.java
@@ -1,53 +1,93 @@
 package com.java;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.ReadOnlyFileSystemException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Map<String, String> hashMapCsvData = readCsvFile("read-data.csv");
+		evaluateCell(hashMapCsvData);
+		Map<String, Integer> valueMap = evaluateCell(hashMapCsvData);
+		System.out.println(hashMapCsvData);
+		System.out.println(valueMap);
+	}
+	
+	
+
+	
+
+	private static Map<String, Integer> evaluateCell(Map<String, String> hashMapCsvData) {
 		// TODO Auto-generated method stub
+		Map<String, Integer> originalValueMap=new HashMap<>();
+		for(Entry<String, String> entry: hashMapCsvData.entrySet()) {
+			String valueEntry = entry.getValue();
+			String getKeyString=entry.getKey();
+			
+			if(valueEntry.startsWith("=")) {
+				String valueExp=valueEntry.substring(1);
+				int calculated=calculateFormula(valueExp,originalValueMap,hashMapCsvData);
+				originalValueMap.put(getKeyString, calculated);
+			}
+			else {
+				 originalValueMap.put(getKeyString,Integer.parseInt(valueEntry));
+			}
+		}
+		return originalValueMap;
+	}
+
+	private static int calculateFormula(String valueExp,Map<String,Integer>originalValueMap,Map<String,String> hashMapCsvData) {
+		   String[] stringsArrRhs =valueExp.split("\\+");
+//		   System.out.println(Arrays.toString(stringsArrRhs));
+		   int add=0;
+		   for(String ch:stringsArrRhs) {
+			   if(hashMapCsvData.containsKey(ch)) {
+				   if(originalValueMap.containsKey(ch)) {
+					   add+=originalValueMap.get(ch);
+				   }
+				   
+			   }else {
+				   add+=Integer.parseInt(ch);
+			   }
+		   }
+				
+		return add;
 		
-		Map<String, String> hashMap = readCsvFile("read-data.csv");
-		System.out.println(hashMap);
 	}
 
 	private static Map<String, String> readCsvFile(String fileName) {
-		Map<String, String> hashMap = null;
-		
+		Map<String, String> hashMapCsvData = null;
+		BufferedReader reader = null;
+
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(fileName));
-			hashMap = new HashMap<>();
+			reader = new BufferedReader(new FileReader(fileName));
+			hashMapCsvData = new HashMap<>();
 			String line;
 			while ((line = reader.readLine()) != null) {
 				String[] strArrStrings = line.split(",");
-
 				int n = strArrStrings.length;
-
 				for (int i = 0; i < n; i++) {
 					String[] eachStrings = strArrStrings[i].split(":");
-
-					hashMap.put(eachStrings[0].trim(), eachStrings[1].trim());
-
+					hashMapCsvData.put(eachStrings[0].trim(), eachStrings[1].trim());
 				}
-
-//				
 			}
 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		return hashMap;
+
+		return hashMapCsvData;
 
 	}
 
